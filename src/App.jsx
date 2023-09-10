a15a385efe1537b385482df2cfa5b09620846156
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-10 02:59:10 -0400

    upload template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Button from "ui/Button"
+
+// Application 
+export default function App () {
+    return (
+      <div className="m-3 d-flex flex-column align-items-center"> 
+        <h1> This is a React App </h1>
+        <Button> Ok! </Button>
+      </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+
+const cfg = require('./config')
+const path = require('path');
+const isProduction = process.env.NODE_ENV == 'production';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    // main file
+    entry: path.join(cfg.SRC, 'index.js'),
+    mode: process.env.NODE_ENV, // production or development
+    
+    // main export file
+    output: {
+        path: cfg.DIST,
+        filename: "bundle.js",
+    },
+    
+    // if start a dev server
+    devServer: {
+        contentBase: cfg.DIST,
+        port: cfg.PORT,
+        host: 'localhost',
+    },
+    
+    // html export
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(cfg.PUBLIC, 'index.html'),
+        }),
+    ],
+    
+    resolve: {
+        // alias imports
+        alias: {
+            "ui": cfg.SRC + "/ui",
+            "utils": cfg.SRC + "/utils",  
+            
+            // programming in mobile apps
+            "eruda": isProduction ? 
+                cfg.SRC + "/utils/__eruda-fake.js" : // remove eruda in production
+                "eruda",
+        },
+        extensions: ["*", ".js", ".jsx"]
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(jpg|png)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+};
